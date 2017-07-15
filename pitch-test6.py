084c53547980581505aba9e5dd41dc136b51c3eb
mxordn <moritzheffter@macbook-air.local>
2017-07-15 15:31:25 +0200

    some progress in handling objects
    The def compare_function yet does not check if the returned value was
    already added.

--- pitch-test6.py
+++ pitch-test6.py
@@ -1,42 +1,50 @@
 from music21 import *
 import inspect
 tharsis = stream.Stream()
-tharsis = converter.parse("12_reges.xml")
+tharsis = converter.parse("12_reges2.xml")
 stmpPart = stream.Stream()
-newPart = []
+newPart = stream.Part()
 os = stream.Part()
 tharsisNotes = tharsis.flat.getElementsByClass(note.Note)
 
-def compare_pitch_high(slice, high):
-	for each in range(len(slice)):
-		if slice[each] > high:
-		    high = slice[each]
-		else:
-		    high = high
-	return high
+def compare_pitch_high(slice):
+    high = slice[0]
+    for each in range(len(slice)):
+        if slice[each] > high:
+            high = slice[each]
+        else:
+            high = high
+    print high
+    return high
 
-def compare_pitch_low(slice, low):
-	for each in range(len(slice)):
+def compare_pitch_low(slice):
+    low = slice[0]
+    for each in range(len(slice)):
 		if slice[each] < low:
 		    low = slice[each]
 		else:
 		    low = low
-	return low
+    return low
 
 for elem in tharsisNotes.flat:
     num = elem.offset
     print num
     slice = tharsisNotes.flat.getElementsByOffset(num, mustBeginInSpan = False, mustFinishInSpan = False)
-    print len(slice)
-    high = slice[0]
     if len(slice) > 1:
-        newPart.append(compare_pitch_high(slice, high))
+        for everye in range(len(slice)):
+            print slice[everye].offset
+            if slice[everye].offset > newPart.highestOffset:
+                newnote = compare_pitch_high(slice)
+                if newnote != newPart.flat.getElementsByOffset(newPart.highestOffset):
+                    newPart.append(newnote)
+            else:
+                print "Nothing to be done here."
     else:
-        newPart.append(high)
+        newPart.append(slice[0])
 #        temp.offset = et.offset
 #        of = temp.offset
     print "--"
 
-print newPart
+#newPart.show()
 #os.append(newPart)
 #os.show()
