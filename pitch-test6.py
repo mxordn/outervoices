84d5df1e63a178a2a7e9e4fb9a916f26ee301466
mxordn <moritzheffter@macbook-air.local>
2017-07-16 00:34:01 +0200

    improved code version ready  for lower voice

--- pitch-test6.py
+++ pitch-test6.py
@@ -3,8 +3,9 @@ import inspect
 tharsis = stream.Stream()
 tharsis = converter.parse("12_reges2.xml")
 stmpPart = stream.Stream()
-newPart = stream.Part()
-os = stream.Part()
+newPartHigh = stream.Part()
+newPartLoW = stream.Part()
+os = stream.Stream()
 tharsisNotes = tharsis.flat.getElementsByClass(note.Note)
 
 def compare_pitch_high(slice):
@@ -26,6 +27,7 @@ def compare_pitch_low(slice):
 		    low = low
     return low
 
+#HIGHEST VOICE
 for elem in tharsisNotes.flat:
     num = elem.offset
     print num
@@ -33,23 +35,25 @@ for elem in tharsisNotes.flat:
     if len(slice) > 1:
         for everye in range(len(slice)):
             print slice[everye].offset
-            if slice[everye].offset > newPart.highestOffset:
+            if slice[everye].offset > newPartHigh.highestOffset:
                 newnote = compare_pitch_high(slice)
-                exnote1 = newPart.flat.getElementsByOffset(newPart.highestOffset, classList= note.Note)
-                exnote2 = note.Note()
+                exnote1 = newPartHigh.flat.getElementsByOffset(newPartHigh.highestOffset, classList= note.Note)
                 exnote2 = exnote1[0]
-                print exnote2
+#                print exnote2
 #                print newnote, exnote[0].pitch
                 if newnote != exnote2:
-                    newPart.append(newnote)
+                    newPartHigh.append(newnote)
             else:
                 print "Nothing to be done here."
     else:
-        newPart.append(slice[0])
+        newPartHigh.append(slice[0])
 #        temp.offset = et.offset
 #        of = temp.offset
     print "--"
 
-newPart.show()
-#os.append(newPart)
-#os.show()
+
+#LOWEST VOICE
+
+os.insert(newPartHigh)
+#os.insert(newPartLow)
+os.show()
